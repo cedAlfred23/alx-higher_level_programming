80de1f7b0621463852ff737aa461735262466453
cedAlfred23 <al.cedricgbaguidi@gmail.com>
2023-11-08 14:00:36 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+"""Defines a class MyList that inherits from list"""
 
-my_list = MyList()
-my_list.append(7)
-# my_list.append('-c')
-# my_list.append('-z')
-# my_list.append('-g')
-# my_list.append('-a')
-# print(my_list)
-my_list.print_sorted()
-# print(my_list)
+
+class MyList(list):
+    """Class that inherits from list.
+
+    Args:
+        list (list): list to sort in ascending order.
+    """
+    def print_sorted(self):
+        """Prints a list in ascending order.
+        """
+        list_ = self[:]
+        list_.sort()
+        print(list_)
